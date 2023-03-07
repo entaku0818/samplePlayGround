79d6e6d620ed6532f038ffc925b62622b20707ee
entaku <entaku19890818@gmail.com>
2023-03-07 09:49:56 +0900

    add Incremental Search

--- MyPlayground.playground/Pages/Incremental Search.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Incremental Search.xcplaygroundpage/Contents.swift
@@ -1,7 +1,51 @@
-//: [Previous](@previous)
 
-import Foundation
+import UIKit
+import SwiftUI
+import PlaygroundSupport
 
-var greeting = "Hello, playground"
+struct ContentView: View {
+    @State private var searchText = ""
+    @State private var selectedItem: Item?
 
-//: [Next](@next)
+    var items = [
+        Item(name: "Apple"),
+        Item(name: "Banana"),
+        Item(name: "Cherry"),
+        Item(name: "Durian"),
+        Item(name: "Elderberry")
+    ]
+
+    var filteredItems: [Item] {
+        if searchText.isEmpty {
+            return items
+        } else {
+            return items.filter { $0.name.contains(searchText) }
+        }
+    }
+
+    var body: some View {
+        VStack {
+            TextField("Search", text: $searchText)
+                .padding(10)
+
+            List(filteredItems) { item in
+                Text(item.name)
+                    .onTapGesture {
+                        selectedItem = item
+                    }
+            }
+
+            if let selectedItem = selectedItem {
+                Text("Selected Item: \(selectedItem.name)")
+            }
+        }
+    }
+}
+
+
+struct Item: Identifiable {
+    var id = UUID()
+    var name: String
+}
+
+PlaygroundPage.current.liveView = UIHostingController(rootView: ContentView().frame(width: 375,height: 600))
