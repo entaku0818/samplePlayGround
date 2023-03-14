13ac8499c36dfff80ae14110379ffcf1a43a425b
TAKUYA ENDO <entaku19890818@gmail.com>
2023-03-14 17:58:34 +0900

    add lottie

--- samplePlayGround/samplePlayGround/ContentView.swift
+++ samplePlayGround/samplePlayGround/ContentView.swift
@@ -11,7 +11,7 @@ import Lottie
 struct ContentView: View {
     var body: some View {
         VStack {
-            LottieView(filename: "OK")
+            AnimationViewControllerWrapper(name: "congratulations")
                 .frame(width: 300, height: 300)
         }
         .padding()
@@ -25,28 +25,5 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct LottieView: UIViewRepresentable {
-    typealias UIViewType = UIView
-    var filename: String
 
-    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
-        let view = UIView(frame: .zero)
-        let animationView = LottieAnimationView()
-        let animation = LottieAnimation.named(filename)
-        animationView.animation = animation
-        animationView.contentMode = .scaleAspectFit
-        animationView.play()
-        animationView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(animationView)
 
-        NSLayoutConstraint.activate([
-            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
-        ])
-
-        return view
-    }
-
-    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
-    }
-}
